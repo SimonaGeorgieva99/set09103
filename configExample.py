6f004d0f67a8457da0856160d7b3c67b2c24b32c
Simona Georgieva <simona_georgieva99@abv.bg>
2020-10-10 10:03:51 +0100

    Add configurations

---
+++ configExample.py
@@ -0,0 +1,34 @@
+import configparser
+from flask import Flask
+
+app=Flask(__name__)
+
+@app.route('/')
+def root():
+	return "Hello from the configuration testing file"
+
+@app.route('/config/')
+def config():
+	s=[]
+	s.append('debug:' + app.config['DEBUG'])
+	s.append('port:' + app.config['port'])
+	s.append('url:' + app.config['url'])
+	s.append('ip_address:' + app.config['ip_address'])
+	return ', '.join(s)
+
+def init(app):
+	config = configparser.ConfigParser()
+	try:
+		config_location="etc/defaults.cfg"
+		config.read(config_location)
+
+		app.config['DEBUG']=config.get("config", "debug")
+		app.config['ip_address']=config.get("config", "ip_address")
+		app.config['port']=config.get("config", "port")
+		app.config['url']=config.get("config", "url")
+	except:
+		print ("Could not read configs from: ", config_location)
+
+if __name__ == "__main__":
+	init(app)
+	app.run(host=app.config['ip_address'], port=int(app.config['port']))
